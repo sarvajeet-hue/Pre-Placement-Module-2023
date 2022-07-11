8ab5251195a6f544b0eab6b2a1cdae15dd9a445a
sarvajeet-hue <75826972+sarvajeet-hue@users.noreply.github.com>
2022-07-11 22:11:42 +0530

    Create Binary_search.cpp

---
+++ ALGO I/Day 1/Binary_search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+        int ans = 0;
+        int mid = start + (end - start)/2;
+        while(start <= end){
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                start = mid + 1;
+            }else{
+                end = mid -1;
+            }mid = start + (end - start)/2;
+        }return -1;
+        
+    }
+};
