b5d40692a2776101fc63730958e7a59bae22dcbf
sarvajeet-hue <75826972+sarvajeet-hue@users.noreply.github.com>
2022-07-12 03:08:36 +0530

    Create Search_insert_position.cpp

---
+++ ALGO I/Day 1/Search_insert_position.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       
+        int n = nums.size();
+       
+        int start = 0;
+        
+        int end = n-1;
+        int mid = start + (end - start)/2;
+        while(start <= end){
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }mid = start + (end - start)/2;
+        }return start;
+    }
+};
